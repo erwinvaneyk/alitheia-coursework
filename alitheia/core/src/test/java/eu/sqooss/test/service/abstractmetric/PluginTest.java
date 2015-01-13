02bdb51522d6fc2d81cf0f9bb87ce0279c8add47
erwinvaneyk <erwinvaneyk@gmail.com>
2015-01-13 21:44:57 +0100

    Integration testing using pax exam

--- alitheia/core/src/test/java/eu/sqooss/test/service/abstractmetric/PluginTest.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/abstractmetric/PluginTest.java
@@ -1,28 +1,53 @@
 package eu.sqooss.test.service.abstractmetric;
 
-import org.apache.sling.testing.mock.osgi.MockOsgi;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.*;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
-import java.util.List;
+import javax.inject.Inject;
+import java.io.File;
 
+//@ExamReactorStrategy(PerClass.class)
+@RunWith(PaxExam.class)
 public class PluginTest {
-    private static ExamplePlugin plugin;
 
-    @BeforeClass
-    public static void setUp() {
-        BundleContext bc = MockOsgi.newBundleContext();
-        plugin = new ExamplePlugin(bc);
+    @Inject
+    BundleContext context;
+
+    @Configuration
+    public Option[] config() {
+        File workingDir = new File(System.getProperty("user.dir"));
+        if(workingDir.getName().equals("core")) {
+            // you are a hack yourself..
+            workingDir = workingDir.getParentFile().getParentFile();
+        }
+        Option[] options = options(
+                mavenBundle("org.osgi", "org.osgi.core", "4.2.0"),
+                mavenBundle("org.apache.felix", "org.apache.felix.webconsole", "1.2.8"),
+                mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.0.8"),
+                mavenBundle("org.apache.felix", "org.apache.felix.http.jetty", "1.0.1"),
+                mavenBundle("org.apache.felix", "org.apache.felix.eventadmin", "1.0.0"),
+                // This is not the dirty hack you are looking for..
+                bundle("file:" + workingDir + "/external/org.hibernate.core/target/org.hibernate-3.5.1-Final.jar"),
+                mavenBundle("eu.sqooss.alitheia", "core", "0.95-SNAPSHOT"),
+                junitBundles()
+        );
+        return options;
     }
 
     @Test
-    public void test1() {
-        assertTrue(true);
+    public void checkInject() {
+        for(Bundle x : context.getBundles()) {
+            System.out.println("Bundle: " + x.getSymbolicName());
+        }
+        assertNotNull(new Object());
     }
 }
