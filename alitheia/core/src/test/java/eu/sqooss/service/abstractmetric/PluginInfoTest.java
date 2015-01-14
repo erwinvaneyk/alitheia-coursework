eb23e1fc8cb666f5c28c7590aea4c4e23d155bca
Tim Rensen <t.rensen@student.tudelft.nl>
2015-01-14 17:07:09 +0100

    Extended test suite

--- alitheia/core/src/test/java/eu/sqooss/service/abstractmetric/PluginInfoTest.java
+++ alitheia/core/src/test/java/eu/sqooss/service/abstractmetric/PluginInfoTest.java
@@ -3,7 +3,6 @@ package eu.sqooss.service.abstractmetric;
 import eu.sqooss.service.db.DAObject;
 import eu.sqooss.service.db.DBService;
 import eu.sqooss.service.db.PluginConfiguration;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -19,18 +18,13 @@ public class PluginInfoTest {
     private PluginInfo pi;
     private Set<PluginConfiguration> spc;
 
-    @Before
-    public void init() {
-        pi = new PluginInfo();
-    }
-
-    private void setSetWithOneElement() {
+    private Set<PluginConfiguration> setWithOneElement() {
         PluginConfiguration pc = mock(PluginConfiguration.class);
 
         spc = new HashSet<>();
         spc.add(pc);
 
-        pi = new PluginInfo(spc);
+        return spc;
     }
 
     @Test
@@ -41,22 +35,49 @@ public class PluginInfoTest {
 
     @Test
     public void constructorWithConfig() {
-        setSetWithOneElement();
+        pi = new PluginInfo(setWithOneElement());
         assertEquals(spc, pi.getConfiguration());
     }
 
     @Test
+    public void constructorWithConfigAndNull() {
+        pi = new PluginInfo(setWithOneElement(), null);
+        assertEquals(spc, pi.getConfiguration());
+    }
+
+    @Test
+    public void constructorWithConfigAndPlugin() {
+        String name = "someName";
+        String version = "1.2.3";
+        Set<Class<? extends DAObject>> activationTypes = new HashSet<>();
+
+        AlitheiaPlugin ap = mock(AlitheiaPlugin.class);
+        when(ap.getName()).thenReturn(name);
+        when(ap.getVersion()).thenReturn(version);
+        when(ap.getActivationTypes()).thenReturn(activationTypes);
+
+        pi = new PluginInfo(setWithOneElement(), ap);
+        assertEquals(spc, pi.getConfiguration());
+        assertEquals(name, pi.getPluginName());
+        assertEquals(version, pi.getPluginVersion());
+        assertEquals(activationTypes, pi.getActivationTypes());
+    }
+
+    @Test
     public void testGetConfPropIdInvalidName() {
+        pi = new PluginInfo();
         assertNull(pi.getConfPropId(null, "type"));
     }
 
     @Test
     public void testGetConfPropIdInvalidType() {
+        pi = new PluginInfo();
         assertNull(pi.getConfPropId("name", null));
     }
 
     @Test
     public void testGetConfPropIdEmptySet() {
+        pi = new PluginInfo();
         assertNull(pi.getConfPropId("name", "type"));
     }
 
@@ -82,6 +103,7 @@ public class PluginInfoTest {
 
     @Test
     public void testUpdateConfigEntryNotFound() throws Exception {
+        pi = new PluginInfo();
         assertFalse(pi.updateConfigEntry(null, "aName", null));
     }
 
@@ -104,5 +126,61 @@ public class PluginInfoTest {
 
         assertTrue(pi.updateConfigEntry(dbservice, "aName", "42"));
         verify(pc).setValue("42");
+    }
+
+    @Test
+    public void testRemoveConfigEntry() throws Exception {
+        // Mock PluginConfiguration
+        PluginConfiguration pc = mock(PluginConfiguration.class);
+        long id = 123;
+        when(pc.getName()).thenReturn("aName");
+        when(pc.getType()).thenReturn("INTEGER");
+        when(pc.getId()).thenReturn(id);
+
+        // Create PluginInfo with this PluginConfiguration
+        Set<PluginConfiguration> set = new HashSet<>();
+        set.add(pc);
+        pi = new PluginInfo(set);
+
+        // Mock DBService and return the mocked PluginConfiguration
+        DBService dbservice = mock(DBService.class);
+        when(dbservice.findObjectById(PluginConfiguration.class, id)).thenReturn(pc);
+        when(dbservice.deleteRecord(pc)).thenReturn(true);
+
+        assertTrue(pi.removeConfigEntry(dbservice, "aName", "INTEGER"));
+    }
+
+
+    /**
+     *    COVERAGE BOOST FTW
+     */
+    @Test
+    public void testGetConfPropIdOneElementNotFound() {
+        PluginConfiguration pc = mock(PluginConfiguration.class);
+        when(pc.getName()).thenReturn("someName");
+        when(pc.getType()).thenReturn("INTEGER");
+        when(pc.getValue()).thenReturn("1");
+
+        // Create PluginInfo with this PluginConfiguration
+        Set<PluginConfiguration> set = new HashSet<>();
+        set.add(pc);
+        pi = new PluginInfo(set);
+
+        assertNull(pi.getConfPropId("someOtherName", "INTEGER"));
+    }
+
+    @Test
+    public void testUpdateConfigEntryNotFound2() throws Exception {
+        PluginConfiguration pc = mock(PluginConfiguration.class);
+        when(pc.getName()).thenReturn("someName");
+        when(pc.getType()).thenReturn("INTEGER");
+
+        // Create PluginInfo with this PluginConfiguration
+        Set<PluginConfiguration> set = new HashSet<>();
+        set.add(pc);
+        pi = new PluginInfo(set);
+
+        assertFalse(pi.updateConfigEntry(null, "aName", null));
+        assertNotNull(pi.toString());
     }
 }
