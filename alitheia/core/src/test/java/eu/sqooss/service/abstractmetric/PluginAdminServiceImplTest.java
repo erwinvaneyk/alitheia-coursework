b94cce6915667f2d6e49a927531fcc8e76bff532
Tim Rensen <t.rensen@student.tudelft.nl>
2015-01-16 14:13:05 +0100

    Added more tests

--- alitheia/core/src/test/java/eu/sqooss/service/abstractmetric/PluginAdminServiceImplTest.java
+++ alitheia/core/src/test/java/eu/sqooss/service/abstractmetric/PluginAdminServiceImplTest.java
@@ -1,7 +1,11 @@
 package eu.sqooss.service.abstractmetric;
 
+import eu.sqooss.core.AlitheiaCore;
 import eu.sqooss.impl.service.abstractmetric.PluginAdminServiceImpl;
 import eu.sqooss.service.logging.Logger;
+import eu.sqooss.service.scheduler.Job;
+import eu.sqooss.service.scheduler.Scheduler;
+import eu.sqooss.service.scheduler.SchedulerException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,12 +17,13 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Plugin.class, PluginAdminServiceImpl.class})
+@PrepareForTest({Plugin.class, AlitheiaCore.class})
 public class PluginAdminServiceImplTest {
 
     PluginAdminServiceImpl pa;
@@ -84,6 +89,49 @@ public class PluginAdminServiceImplTest {
         pa.setInitParams(bc, logger);
 
         assertTrue(pa.installPlugin(serviceId));
+    }
+
+
+    @Test
+    public void uninstallPluginLongNull() {
+        Long serviceId = null;
+
+        AlitheiaCore ac = mock(AlitheiaCore.class);
+        Scheduler s = mock(Scheduler.class);
+        when(ac.getScheduler()).thenReturn(s);
+
+        mockStatic(AlitheiaCore.class);
+        when(AlitheiaCore.getInstance()).thenReturn(ac);
+
+        assertTrue(pa.uninstallPlugin(serviceId));
+    }
+
+    @Test
+    public void uninstallPluginLongException() throws Exception{
+        Long serviceId = null;
+
+        AlitheiaCore ac = mock(AlitheiaCore.class);
+        Scheduler s = mock(Scheduler.class);
+        when(ac.getScheduler()).thenReturn(s);
+        doThrow(new SchedulerException("")).when(s).enqueue(any(Job.class));
+
+        mockStatic(AlitheiaCore.class);
+        when(AlitheiaCore.getInstance()).thenReturn(ac);
+
+        ServiceReference[] srs = new ServiceReference[1];
+        ServiceReference sr = mock(ServiceReference.class);
+        srs[0] = sr;
+
+        AlitheiaPlugin ap = mock(AlitheiaPlugin.class);
+
+        BundleContext bc = mock(BundleContext.class);
+        when(bc.getServiceReferences(anyString(), anyString())).thenReturn(srs);
+        when(bc.getService(any(ServiceReference.class))).thenReturn(ap);
+
+        Logger logger = mock(Logger.class);
+        pa.setInitParams(bc, logger);
+
+        assertFalse(pa.uninstallPlugin(serviceId));
     }
 
     /**
